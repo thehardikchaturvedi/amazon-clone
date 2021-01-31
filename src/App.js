4057b3c129691a2ff9ed23d9aa6ef9ae463e11ea
thehardikchaturvedi <hardikx2000@gmail.com>
2021-01-31 15:20:52 +0530

    App Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { useEffect } from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { useStateValue } from "./utils/StateProvider";
-import { auth } from "./utils/firebase";
-import Carousel from "./components/carousel/Carousel";
-import Header from "./components/header/Header";
-import Home from "./components/home/Home";
-import Login from "./components/login/Login";
-import Checkout from "./components/checkout/Checkout";
-
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+);
 
 function App() {
-  const [{ basket, user }, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
+
   useEffect(() => {
+    // will only run once when the app component loads...
+
     auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
       if (authUser) {
+        // the user just logged in / the user was logged in
+
         dispatch({
           type: "SET_USER",
           user: authUser,
         });
       } else {
+        // the user is logged out
         dispatch({
           type: "SET_USER",
           user: null,
@@ -32,15 +46,22 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
-          </Route>
-          <Route path="/carousel">
-            <Carousel />
           </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
-import App from "./App";
-import { StateProvider } from "./utils/StateProvider";
-import reducer, { initialState } from "./utils/reducer";
-
 import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,4 +14,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
